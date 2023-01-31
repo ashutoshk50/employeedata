566389b1496bac79c9fb218a2abd5b23c3619939
ASHUTOSH KUMAR <53807296+ashutoshk50@users.noreply.github.com>
2023-01-31 11:32:08 +0530

    Update conn.js

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/Jhonson", {
+// mongoose.connect("mongodb://localhost:27017/Jhonson", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(()=>{
+//     console.log('Connection successfull');
+// }).catch((err)=>{
+//     console.log('Connection not successfull' + err);
+// });
+
+mongoose.set('strictQuery', true);
+
+const database = `mongodb+srv://server01:1%40server01@cluster0.gqrhnix.mongodb.net/registrationData?retryWrites=true&w=majority`;
+
+
+mongoose.connect(database, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
